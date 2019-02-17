f14cdb391c749993556256053b72a206aa0e845b
fatmanurvurgun <45992667+fatmanurvurgun@users.noreply.github.com>
2019-02-17 15:51:22 +0300

    Ders classı eklendi

---
+++ BilgiYonetimSistemi.DATA/Ders.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgiYonetimSistemi.DATA
+{
+    public class Ders
+    {
+        public int DersID { get; set; }
+
+        public string DersKodu { get; set; }
+
+        public int Kredisi { get; set; }
+
+    }
+}
