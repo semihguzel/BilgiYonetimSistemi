74e06476ec2b6930029ca8be25cb10f4ca48ea6c
Semih Guzel <semih_guzel@outlook.com>
2019-02-22 23:19:59 +0300

    Yonetici BLL katmani yapildi.

---
+++ BilgiYonetimSistemi.BLL/Repository/Concrete/YoneticiConcrete.cs
@@ -0,0 +1,28 @@
+﻿using BilgiYonetimSistemi.BLL.Repository.Abstract;
+using BilgiYonetimSistemi.DAL;
+using BilgiYonetimSistemi.DATA.Entities;
+using RepositoryPattern.BLL.Repository.Abstract;
+using RepositoryPattern.BLL.Repository.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgiYonetimSistemi.BLL.Repository.Concrete
+{
+    public class YoneticiConcrete : IYonetici
+    {
+        public IRepository<Yonetici> _yoneticiRepository;
+        public IUnitOfWork _yoneticiUnitOfWork;
+        private DbContext _dbContext;
+
+        public YoneticiConcrete()
+        {
+            _dbContext = new Context();
+            _yoneticiUnitOfWork = new EFUnitOfWork(_dbContext);
+            _yoneticiRepository = _yoneticiUnitOfWork.GetRepository<Yonetici>();
+        }
+    }
+}
